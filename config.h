beafd6d453ee126a41e43e2f30d6a84b07571e6f
Scott Gullaksen <scott.gullaksen@drift.oslo.kommune.no>
2022-08-22 09:36:56 +0200

    Peke på egne scripts

--- config.h
+++ config.h
@@ -1,16 +1,14 @@
 #define CMDLENGTH 45
-#define DELIMITER "  "
+#define DELIMITER "     "
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
-	BLOCK("sb-mail",    1800, 17),
-	BLOCK("sb-music",   0,    18),
-	BLOCK("sb-disk",    1800, 19),
-	BLOCK("sb-memory",  10,   20),
-	BLOCK("sb-loadavg", 5,    21),
-	BLOCK("sb-mic",     0,    26),
-	BLOCK("sb-record",  0,    27),
-	BLOCK("sb-volume",  0,    22),
-	BLOCK("sb-battery", 5,    23),
-	BLOCK("sb-date",    1,    24)
+//	BLOCK("/home/scottg/scripts/statusbar/wttr",    3600,    8),
+	BLOCK("/home/scottg/scripts/statusbar/vol",    1,    7),
+	BLOCK("/home/scottg/scripts/statusbar/nwrk",    10,    6),
+//	BLOCK("/home/scottg/scripts/statusbar/hdd",    60000,    5),
+//	BLOCK("/home/scottg/scripts/statusbar/cpu",    5000,    4),
+	BLOCK("/home/scottg/scripts/statusbar/mem",    1,    3),
+	BLOCK("/home/scottg/scripts/statusbar/bat",    1,    2),
+	BLOCK("/home/scottg/scripts/statusbar/clock",    1,    1)
 };
