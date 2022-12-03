42ad729df7e7d40dfdfe06e34e0743932b4f9daf
Scott Gullaksen <scott.gullaksen@gmail.com>
2022-12-03 14:43:19 +0100

    Decreased delimitter length

--- config.h
+++ config.h
@@ -1,14 +1,11 @@
 #define CMDLENGTH 45
-#define DELIMITER "     "
+#define DELIMITER "  "
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
-//	BLOCK("/home/scottg/scripts/statusbar/wttr",    3600,    8),
-	BLOCK("/home/scottg/scripts/statusbar/vol",    1,    7),
-	BLOCK("/home/scottg/scripts/statusbar/nwrk",    10,    6),
-//	BLOCK("/home/scottg/scripts/statusbar/hdd",    60000,    5),
-//	BLOCK("/home/scottg/scripts/statusbar/cpu",    5000,    4),
-	BLOCK("/home/scottg/scripts/statusbar/mem",    1,    3),
-	BLOCK("/home/scottg/scripts/statusbar/bat",    1,    2),
-	BLOCK("/home/scottg/scripts/statusbar/clock",    1,    1)
+	BLOCK("$HOME/scripts/statusbar/vol",    1,    7),
+	BLOCK("$HOME/scripts/statusbar/nwrk",    10,    6),
+	BLOCK("$HOME/scripts/statusbar/mem",    1,    3),
+	BLOCK("$HOME/scripts/statusbar/bat",    1,    2),
+	BLOCK("$HOME/scripts/statusbar/clock",    1,    1)
 };
